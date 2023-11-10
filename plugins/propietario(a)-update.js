f881ecf356e960724bb9072e4af26f081a8bef98
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2023-11-10 03:10:18 -0800

    Actualizar propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/KatashiFukushima/KatashiBot-MD.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/AngelPeruFachero/Broly-Bot-MD.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
