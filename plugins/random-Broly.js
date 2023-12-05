2f3dcb4b19b1236418afa0f694ac64a41aeac5e1
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2023-12-05 16:11:57 -0500

    Actualizar plugins random-Broly.js

---
+++ plugins/random-Broly.js
@@ -0,0 +1,13 @@
+let handler = async(m, { conn, text, command }) => {
+let yh = global.broly
+let url = yh[Math.floor(Math.random() * yh.length)]
+conn.sendMessage(m.chat, { image: { url: url }, caption: "*_AY ESTA LA IMAGEN DE BROLY_*" }, { quoted: m });
+}
+handler.command = /^(broly)$/i
+handler.tags = ['anime']
+handler.help = ['imgbroly']
+export default handler
+
+global.broly = [
+"./media/menus/Broly1.jpg","./media/menus/Broly2.jpg","./media/menus/Broly3.jpg","./media/menus/Broly4.jpg","./media/menus/Broly5.jpg","./media/menus/Broly6.jpg","./media/menus/Broly7.jpg","./media/menus/Broly8.jpg","./media/menus/Broly9.jpg","./media/menus/Broly10.jpg","./media/menus/Broly11.jpg","./media/menus/Broly12.jpg","./media/menus/Broly13.jpg","./media/menus/Broly14.jpg","./media/menus/Broly15.jpg","./media/menus/Broly16.jpg","./media/menus/Broly17.jpg","./media/menus/Broly18.jpg","./media/menus/Broly19.jpg"
+]
