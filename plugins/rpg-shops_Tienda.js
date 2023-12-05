8e931efb3c724083378793a8dd4b96f83319ffc5
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2023-12-04 20:50:31 -0500

    Actualizar rpg-shops_Tienda.js

--- plugins/rpg-shops_Tienda.js
+++ plugins/rpg-shops_Tienda.js
@@ -8,9 +8,7 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 }
 let grupos = [nna, nn, nnn, nnnt]
 let gata = [img5, img6, img7, img8, img9]
-let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🥷🏻', body: 'support group' , sourceUrl: grupos.getRandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
-let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: ' 𝗦𝘂𝗽𝗲𝗿 𝗕𝗿𝗼𝗹𝘆 𝗕𝗼𝘁 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(global.img)).buffer(), sourceUrl: yt }}}
-let dos = [enlace, enlace2]
+
 
 const items = {
    buy: {
