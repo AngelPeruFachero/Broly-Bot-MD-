f923876e99dfe04cab2e5a30ab8a8cd3cfa5f099
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2023-12-05 18:23:31 -0500

    Actualizar random-Broly.js

--- plugins/random-Broly.js
+++ plugins/random-Broly.js
@@ -1,13 +1,13 @@
 let handler = async(m, { conn, text, command }) => {
-let yh = global.ibroly
+let yh = global.broly
 let url = yh[Math.floor(Math.random() * yh.length)]
-conn.sendMessage(m.chat, { image: { url: url }, caption: "*_AQUI ESTÁ LA IMAGEN_*" }, { quoted: m });
+conn.sendMessage(m.chat, { image: { url: url }, caption: "*_Aqui tienes tu pedido_*" }, { quoted: m });
 }
-handler.command = /^(ibroly)$/i
+handler.command = /^(broly)$/i
 handler.tags = ['anime']
-handler.help = ['ibroly']
+handler.help = ['broly']
 export default handler
 
-global.ibroly = [
+global.broly = [
 "./media/menus/Broly1.jpg","./media/menus/Broly2.jpg","./media/menus/Broly3.jpg","./media/menus/Broly4.jpg","./media/menus/Broly5.jpg","./media/menus/Broly6.jpg","./media/menus/Broly7.jpg","./media/menus/Broly8.jpg","./media/menus/Broly9.jpg","./media/menus/Broly10.jpg","./media/menus/Broly11.jpg","./media/menus/Broly12.jpg","./media/menus/Broly13.jpg","./media/menus/Broly14.jpg","./media/menus/Broly15.jpg","./media/menus/Broly16.jpg","./media/menus/Broly17.jpg","./media/menus/Broly18.jpg","./media/menus/Broly19.jpg"
 ]
