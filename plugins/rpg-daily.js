c26728781ba0068b423f0abf92444062c57c4ab8
AngelPeruFachero <149626423+AngelPeruFachero@users.noreply.github.com>
2023-12-07 18:19:56 -0500

    Actualizar rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -4,16 +4,13 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 }
 let grupos = [nna, nn, nnn, nnnt]
 let gata = [img5, img6, img7, img8, img9]
-let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🐈', body: 'support group' , sourceUrl: grupos.getRandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
-let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(global.img)).buffer(), sourceUrl: yt }}}
-let dos = [enlace, enlace2]    
 
 let user = global.db.data.users[m.sender]
 let premium = user.premium
 
 let exp = `${pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800])}` * 1
 let exppremium = `${pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500])}` * 1
-	
+
 let money = `${pickRandom([300, 500, 700, 900, 500, 800, 900, 1100, 1350, 1500])}` * 1
 let moneypremium = `${pickRandom([800, 1300, 1600, 1900, 2200, 2500, 2700, 3000, 3300, 3500])}` * 1
 
@@ -31,11 +28,11 @@ let umpanpremium = `${pickRandom([30, 60, 90, 120, 150, 180, 210, 240, 270, 300]
 
 //let gata = Math.floor(Math.random() * 2000)
 
-const recompensas = {	
+const recompensas = {        
   exp: premium ? exppremium : exp,
   money: premium ? moneypremium : money,
   potion: premium ? potionpremium : potion,
-  tiketcoin: premium ? tiketcoinpremium : tiketcoin,	
+  tiketcoin: premium ? tiketcoinpremium : tiketcoin,        
   eleksirb: premium ? eleksirbpremium : eleksirb,
   umpan: premium ? umpanpremium : umpan,
 }
