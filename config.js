ca2febf366c5a4d3681c0db218260edae5fcc6e2
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-12-18 15:19:48 -0600

    Broly

--- config.js
+++ config.js
@@ -175,19 +175,19 @@ global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.pre
 
 //⊱ ━━━━━.⋅ IMG ⋅.━━━━ ⊰
 
-global.imagen1 = fs.readFileSync("./media/menus/Menu3.jpg")
-global.imagen2 = fs.readFileSync("./media/menus/img1.jpg")
-global.imagen3 = fs.readFileSync("./media/menus/img2.jpg")
-global.imagen4 = fs.readFileSync("./media/menus/img3.jpg")
-global.imagen5 = fs.readFileSync("./media/menus/img4.jpg")
-global.imagen6 = fs.readFileSync("./media/menus/img5.jpg")
-global.imagen7 = fs.readFileSync("./media/menus/img6.jpg")
-global.imagen8 = fs.readFileSync("./media/menus/img7.jpg")
-global.imagen9 = fs.readFileSync("./media/menus/img8.jpg")
-global.imagen10 = fs.readFileSync("./media/menus/img9.jpg")
-global.imagen11 = fs.readFileSync("./media/menus/img10.jpg")
-global.imagen12 = fs.readFileSync("./media/menus/img11.jpg")
-global.imagen13 = fs.readFileSync("./media/menus/img12.jpg")
+global.imagen1 = fs.readFileSync("./media/menus/Menu1.jpg")
+global.imagen2 = fs.readFileSync("./media/menus/Menu2.jpg")
+global.imagen3 = fs.readFileSync("./media/menus/Menu3.jpg")
+global.imagen4 = fs.readFileSync("./media/menus/Menu4.jpg")
+global.imagen5 = fs.readFileSync("./media/menus/Menu5.jpg")
+global.imagen6 = fs.readFileSync("./media/menus/Menu6.jpg")
+global.imagen7 = fs.readFileSync("./media/menus/Menu7.jpg")
+global.imagen8 = fs.readFileSync("./media/menus/Menu8.jpg")
+global.imagen9 = fs.readFileSync("./media/menus/Menu9.jpg")
+global.imagen10 = fs.readFileSync("./media/menus/Menu10.jpg")
+global.imagen11 = fs.readFileSync("./media/menus/Menu11.jpg")
+/*global.imagen12 = fs.readFileSync("./media/menus/img11.jpg")
+global.imagen13 = fs.readFileSync("./media/menus/img12.jpg")*/
 
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
@@ -223,8 +223,8 @@ global.logogit = 'https://telegra.ph/file/75bbb5686e0dff952e79c.jpg'
 global.welgata = [ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb]
 global.redesMenu = [cnl, nna, nn, md, ig, paypal, yt, asistencia, fb]
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18]
-global.gataVidMenu = ['./media/menus/Menuvid1.mp4', './media/menus/Menuvid2.mp4', './media/menus/Menuvid3.mp4']
-global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13]
+//global.gataVidMenu = ['./media/menus/Menuvid1.mp4', './media/menus/Menuvid2.mp4', './media/menus/Menuvid3.mp4']
+global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11]
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
