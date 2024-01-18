ad1744769a27bde438958351e4ad02ae4837dab6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-18 07:08:37 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['DiegoOficial', 'Edge', '2.0.0'] : methodCodeQR ? ['KatashiBot-MD', 'Edge', '2.0.0'] : ['DiegoOficial', '', ''],
+browser: opcion == '1' ? ['DiegoOficial', 'Edge', '2.0.0'] : methodCodeQR ? ['DiegoOficial', 'Edge', '2.0.0'] : ['DiegoOficial', '', ''],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
